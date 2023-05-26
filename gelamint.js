383fb205eb3aa75af2604e257a480e97d8f0e911
diopside <nick@diopside.io>
2023-05-25 22:25:25 -0500

    it actually should work now
    - added a versatile 'addListener' function that streamlines working w observers and eventlisteners
    - removed some old incorrect Class name identifiers
    - corrected some inconsistent variable identifiers in GelSheet class that were breaking

--- gelamint.js
+++ gelamint.js
@@ -23,7 +23,7 @@ export class GelSheet extends CSSStyleSheet {
    // Removes the css for the indicated property from a larger block of css rule txt
    static removeStyleFromRuleText(rTxt, prop, i=rTxt.indexOf(prop)) { 
       return rTxt.replace(rTxt.substring(i, (rTxt.indexOf(';', i+1))+1) ,'')
-   }
+   } 
 
    // for converting between DOM/JS and HTML style property identifiers
    static camelCaseToDashSep(str) {
@@ -44,8 +44,10 @@ export class GelSheet extends CSSStyleSheet {
    } 
 
    constructor(sheetName, initObjOrTxt) { super(); 
+ 
       this.noCache = initObjOrTxt.noCache === true  
       this.identifier = sheetName; 
+ 
       if (sheetName !== undefined) { 
          // dont want to override existing sheets if for some reason
          // duplicate names are passed, so we + an incrementing # to the end
@@ -58,11 +60,12 @@ export class GelSheet extends CSSStyleSheet {
          GelSheet.cache[this.identifier] = this
       }
       //
-      if (typeof initObjOrTxt === 'str'
-         || 'content' in initObjOrTxt) {
+      if (typeof initObjOrTxt === 'string'
+         || (typeof initObjOrTxt === 'object'
+             && 'content' in initObjOrTxt)) {
          // if str content was passed to constructor, 
          // apply it to the new css sheet
-         this.replaceSync(initRuleTxt) //CSSStyleSheet api
+         this.replaceSync(initObjOrTxt.content ?? initObjOrTxt) //CSSStyleSheet api
       }
    }
 
@@ -127,6 +130,15 @@ export class GelSheet extends CSSStyleSheet {
 
 
 
+ 
+
+
+
+
+
+
+
+
 
 // Gelamint - A handy base web-component class that all others can inherit from
 // uses constructable stylesheets API for style scoping and shadow DOM for templating
@@ -134,12 +146,95 @@ export class Gelamint extends HTMLElement {
 
    static defaultCss = getDefaultCss()
 
-   #template;
-   #uniqueId;
-
-   #sheets = {
-      default: this.makeSheet('default', Gelamint.defaultCss)
+   static throttle(func, time) { 
+      let throttled = false,
+         savedState = [];
+    
+      function throttler() { 
+         if (throttled) 
+            return savedState = [this, arguments] 
+         throttled = true  
+         func.apply(this, arguments)  
+         setTimeout( function() {
+            throttled = false 
+            if (savedState[1]) {  
+               throttler.apply(...savedState) 
+               savedState = [] 
+            }
+         }, time) 
+      } 
+      return throttler  
    }
+
+   static svg = {
+      textClass: 'tl-responsive-text',
+      tags: ['svg','circle','text','path','g','rect','ellipse'],
+      attrs: {
+         default:{
+            'height':'100%',  'width': '100%',
+            'version':'1.1',  'preserveAspectRatio':'xMidYMid meet',
+            'xmlns':'http://www.w3.org/2000/svg',  'stroke-linejoin':'round',
+            'xml:space':'preserve',  'stroke-miterlimit':'2',
+            'fill-rule':'evenodd',  'clip-rule':'evenodd' 
+         },
+         text: {
+            'x':'50%',  'y':'50%',  'font-size':'100px',
+            'dominant-baseline':'middle',  'text-anchor':'middle'
+         }  
+      },
+      txtMeasureContainerStyle: (font) => `
+         font-family: ${font};  font-size: 100px!important;
+         position: absolute!important;  top: -10000%;  left:-10000%;`,
+   }  
+
+   static setElAttrs(el, attrDict) {
+      Object.entries(attrDict).forEach(([key,val]) => el.setAttribute(key, val)) 
+   }
+
+   static makeElement(tag, innerContent, initAttrs ) {    
+      const newEl = Gelamint.svg.tags.includes(tag) ?
+         document.createElementNS('http://www.w3.org/2000/svg', tag) :
+         document.createElement(tag) 
+      newEl.innerHTML = innerContent ?? '';
+      if (initAttrs) {
+         Gelamint.setElAttrs(newEl, (tag === 'svg' ?
+            { ...Gelamint.svg.attrs.default, ...initAttrs } :
+            initAttrs));
+      }
+      return newEl
+   }   
+
+   static observeOnOffScreen(leftCallback, returnedCallback) {
+      return new IntersectionObserver( entries => {    
+         if (entries[0].intersectionRatio <= 0) leftCallback(); 
+         else                                   returnedCallback(); 
+      })
+   } 
+
+   static buildSvgTextNode(text, fontFamily, attrs) {  
+      const svg = Gelamint.makeElement('svg',
+            Gelamint.makeElement('text', text, Gelamint.svg.attrs.text).outerHTML,
+            {  'class': Gelamint.svg.textClass  } 
+         ),
+         container = Gelamint.makeElement('div',
+            text,
+            {  'style': Gelamint.svg.txtMeasureContainerStyle(fontFamily)  }
+         ) 
+      document.lastChild.appendChild(container);
+      svg.setAttribute('viewBox', `0 0 ${container.offsetWidth + 20} 100`) ;
+      container.remove(); 
+      
+      if (arguments.length >2) {  Gelamint.setElAttrs(svg, attrs)  }
+
+      return svg
+    } 
+
+   
+
+   #template;
+   #uniqueId; 
+   #sheets;
+   #observers = {temp:{}};
 
    attr = {
       // a person can only type the word 'attribute' out so many times...
@@ -171,13 +266,17 @@ export class Gelamint extends HTMLElement {
 
    constructor() {
       super();
+
+      this.#sheets = {
+         default: this.makeSheet('default', Gelamint.defaultCss)
+      }  
       if (this.attr.has('uid')) { 
          // if not, uniqueId will be set after first triggering its 'get' accessor   
          this.uniqueId = this.attr.get('uid')
       }
       this.hostSelector = `:host([uid="${this.uniqueId}"])` // for scoping style rules
 
-      // init the shadow DOM, immediately applying the default CSS stylings to it
+      // init the shadow DOM, immediately applying the default CSS stylings to it 
       this.attachShadow({ mode: 'open' }).adoptedStyleSheets = [ this.#sheets.default ]; 
    } 
    
@@ -208,20 +307,21 @@ export class Gelamint extends HTMLElement {
    // - a function, to interpolate new values into template content w/ each stamping
    // - an obj, w/ content: as the generating function and other misc options
    set template(contentGen) {
+      console.log(this.#template);
       if (typeof contentGen === 'function') {
-         this.#template = {
+         this.#template = { 
             generate: contentGen,
             fallbacks: null,
             lastRender: ''
          } 
       } else if (typeof contentGen === 'object' && 'content' in contentGen){
-         this.#template = {
+         this.#template = { 
             generate: contentGen.content,
             fallbacks: contentGen.fallbacks,
             lastRender: ''
          } 
       } else {
-         this.#template = {
+         this.#template = { 
             generate: () => contentGen,
             staticTemplate: true,
             fallbacks: null,
@@ -240,20 +340,110 @@ export class Gelamint extends HTMLElement {
 
    get slotNodes()  { return this.getSlotNodes() } 
 
-   makeSheet(name, content) {   
+   newCustomEvent(name, detail){
+      return new CustomEvent(name, 
+         {
+            composed:true,
+            bubbles:true,
+            detail: detail ?? this.name
+         })
+   }
 
+   getElement(strOrEl) {
+      return strOrEl instanceof HTMLElement ? strOrEl :
+         this.shadowRoot.querySelector(strOrEl)
+   }
+
+   #initIntersectionObserver(target) {  
+      if (!this.#observers.intersection) {
+         this.#observers['intersection'] = {}
+      }
+      const funcs = Object.fromEntries(
+         Object.entries(this.#observers.temp)
+            .filter(([name,_]) =>
+               (['screenLeave','screenReturn'].includes(name)))),            
+         key = target.localName+'_'+target.id;
+          
+      this.#observers.intersection[key] = {
+         target,
+         observer: Gelamint.observeOnOffScreen(
+            funcs.screenLeave ?? (()=>{}),
+            funcs.screenReturn ?? (()=>{})
+         )
+      };
+      this.#observers.intersection[key].observer.observe(target)
+      this.#observers.temp = {}
+   }
+
+   timer = 0;
+
+
+   // One stop shop for adding event listeners and observers
+   
+   // if an object is passed in, it will use the keys of the obj
+   // for the event types, and their values as the associated functions
+   // if no string selector is passed in with the object, the listeners
+   // will be applied to 'this'
+
+   // if only 1 argument is passed and its a function, its applied as a 'click'
+   // listener to this.
+   // If a string or strings are included w a function, 
+   // it tries to identify which would be an eventType and which would be a 
+   // element selector, and applies the listeners appropriately
+   addListener(...args) {   
+
+      const objInd = args.length === 3 ? -1 :
+         args.findIndex(a=>typeof a==='object'); 
+
+      if (objInd >= 0) {
+         const elem = args.length === 1 ? this : this.getElement(args[0]); 
+         return  Object.entries(args[objInd])
+                  .map(([eType,eFunc]) => [ elem, eType, eFunc ])
+                  .forEach( argGroup => this.addListener(...argGroup));
+      } 
+
+      const funcInd = args.findIndex( a => typeof a==='function'),
+            selIsEl = args[0] instanceof HTMLElement;  
+
+      let selector, eventType; 
+
+      if (selIsEl)            { [selector, eventType] = [args[0], args[1]] } 
+      else if (funcInd === 0) { [selector, eventType] = [this, 'click']    }
+      else {
+         const strs = args.filter(a=>typeof a==='string'),
+              selAt = strs.findIndex(
+                  str => str.replace(/[^A-Za-z0-9]/g,'').length!==str.length);  
+         selector = selAt >= 0 ? strs.splice(selAt, 1)[0] : this 
+         eventType = strs.length === 1 ? strs[0] : 'click' 
+      } 
+
+      if (  eventType === 'screenLeave'
+         || eventType === 'screenReturn') {   
+         this.#observers.temp[eventType] = args[funcInd] 
+         if (this.timer) clearTimeout(this.timer)
+         this.timer = setTimeout( () => {
+            this.#initIntersectionObserver(this.getElement(selector))
+         }, 20)
+      } 
+      else { 
+         this.getElement(selector).addEventListener(eventType, args[funcInd])   
+      } 
+   }
+
+   makeSheet(name, content) {    
       const ident = (this.localName + '_')    
          + (this.title ? this.title + '_' : ``) 
-         + (name ?? this.uniqueId)
+         + (name ?? this.uniqueId);
 
-      if (GelSheet.cache[ident] && GelSheet.cache[ident].noCache === false) {
-         console.log('using cached Stylesheet for: "'+ident+'"');
+      if (GelSheet.cache[ident] && GelSheet.cache[ident].noCache === false) { 
          return GelSheet.cache[ident]
       }
       return new GelSheet(ident, content)  
    }
 
-   #applyStyleSheet(newSheet) {
+   // append the passed CSSStyleSheet to the 
+   // read-only adoptedStyleSheets array
+   #applyStyleSheet(newSheet) { 
       this.shadowRoot.adoptedStyleSheets = [
          ...this.shadowRoot.adoptedStyleSheets,
          newSheet
@@ -282,7 +472,7 @@ export class Gelamint extends HTMLElement {
    #initRelayProp(propDef) { 
 
       const self = this,
-         aliases = TlComponent.styleAliases,
+         aliases = Gelamint.styleAliases,
             prop = propDef.prop,
             opts = propDef.options,
          targets = prop in aliases ? Object.keys(aliases[prop]) : [].concat(prop);
@@ -363,7 +553,9 @@ export class Gelamint extends HTMLElement {
                .map((param,ind) => _params[ind] ?? param)
                .map((param) => typeof param === 'function' ? param() : param)
             : []; 
+
       newT.innerHTML = this.#template.lastRender = this.#template.generate(params);
+      console.log(this.#template.lastRender);
       return newT.content.cloneNode(true) 
    }
 
@@ -377,6 +569,14 @@ export class Gelamint extends HTMLElement {
       );
       return this.shadowRoot
    }  
+
+   disconnectedCallback() { 
+      Object.entries(this.#observers).forEach(
+         ([key, set]) => Object.entries(set).forEach(
+            ([key, obs]) => obs.observer.disconnect(obs.target) )
+      ) 
+      this.#observers = {}
+   }
 }
 
  
